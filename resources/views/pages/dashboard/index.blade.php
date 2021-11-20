f1b63049f744cbb659d1e56f054ba7c8209ce2ef
alvinmr <maulanaalvin1@gmail.com>
2021-11-20 15:21:51 +0800

    setting for dashboard stisla

---
+++ resources/views/pages/dashboard/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium maxime eos illum incidunt magni hic
+                porro saepe
+                ipsam ex. Quo officia placeat illum, totam ab similique quibusdam alias sed sit.</p>
+        </div>
+    </div>
+@endsection
