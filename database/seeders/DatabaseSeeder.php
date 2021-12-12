f8cfba3a3befa452107a0bfdd0032aed5329390d
Alvin <maulanaalvin1@gmail.com>
2021-12-12 21:41:29 +0800

    add mahasiswa seeder on seed calling

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProdiSeeder::class,
             DosenSeeder::class,
-            PegawaiSeeder::class
+            PegawaiSeeder::class,
+            MahasiswaSeeder::class
         ]);
     }
 }
