d6305b0a1287679aa0a947915df806f819cb3867
Alvin <maulanaalvin1@gmail.com>
2021-12-12 20:27:03 +0800

    add dosen seeder

---
+++ database/seeders/DosenSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Dosen;
+use Illuminate\Database\Seeder;
+
+class DosenSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $nama = ['Abel', 'Rico', 'Ngurah', 'Fernando'];
+        $nip = ['198507232020072001', '198507232020072002', '198507232020072003', '198507232020072004'];
+
+        for ($i = 0; $i < 3; $i++) {
+            Dosen::insert([
+                'nip' => $nip[$i],
+                'nama' => $nama[$i],
+                'email' => strtolower($nama[$i]) . '@gmail.com',
+                'telepon' => '082348758492'
+            ]);
+        }
+    }
+}
