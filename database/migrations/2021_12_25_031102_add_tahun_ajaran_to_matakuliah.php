0396f5ed19a30ed7bce33e713820257c7f6c1d60
Alvin <maulanaalvin1@gmail.com>
2021-12-25 03:18:12 +0800

    tambahin tabel tahun ajaran di matakuliah

---
+++ database/migrations/2021_12_25_031102_add_tahun_ajaran_to_matakuliah.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTahunAjaranToMatakuliah extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('matakuliah', function (Blueprint $table) {
+            $table->string('tahun_ajaran');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('matakuliah', function (Blueprint $table) {
+            $table->dropColumn('tahun_ajaran');
+        });
+    }
+}
