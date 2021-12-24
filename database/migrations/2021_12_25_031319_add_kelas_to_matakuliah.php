8d56dcc310d717d3a241142d9a2a8efd1b2383c7
Alvin <maulanaalvin1@gmail.com>
2021-12-25 03:17:57 +0800

    tambahin field kelas di matakuliah

---
+++ database/migrations/2021_12_25_031319_add_kelas_to_matakuliah.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddKelasToMatakuliah extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('matakuliah', function (Blueprint $table) {
+            $table->string('kelas')->after('jumlah_sks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('matakuliah', function (Blueprint $table) {
+            $table->dropColumn('kelas');
+        });
+    }
+}
