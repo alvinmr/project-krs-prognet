90160e5f3fd9e35db4d3ae0ea73fff77e822ad56
Alvin <maulanaalvin1@gmail.com>
2022-01-03 03:16:41 +0800

    add menu tambahan

--- resources/views/includes/pegawai/sidebar.blade.php
+++ resources/views/includes/pegawai/sidebar.blade.php
@@ -8,12 +8,12 @@
             <a href="index.html">KRSan</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="menu-header">Menu</li>
             <li class="{{ Request::is('*/dashboard') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('pegawai.dashboard') }}"><i class="fas fa-fire"></i>
                     <span>Dashboard</span>
                 </a>
             </li>
+            <li class="menu-header">Master Data</li>
             <li class="{{ Request::is('*/mahasiswa/*') || Request::is('*/mahasiswa') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('pegawai.mahasiswa.index') }}"><i class="fas fa-users"></i>
                     <span>Data Mahasiswa</span>
@@ -31,18 +31,24 @@
                     <span>Data Dosen</span>
                 </a>
             </li>
-            <li class="{{ Request::is('*/krs/*') || Request::is('*/krs') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('pegawai.krs-index') }}"><i
-                        class="fas fa-book"></i>
-                    <span>Data KRS Mahasiswa</span>
-                </a>
-            </li>
             <li class="{{ Request::is('*/tahunajaran/*') || Request::is('*/tahunajaran') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('pegawai.tahunajaran-index') }}"><i
-                        class="fas fa-book"></i>
+                        class="fas fa-calendar"></i>
                     <span>Data Tahun Ajaran</span>
                 </a>
             </li>
+            <li class="menu-header">KRS</li>
+            <li class="{{ Request::is('*/krs/*') || Request::is('*/krs') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('pegawai.krs-index') }}"><i class="fas fa-book"></i>
+                    <span>Data KRS Mahasiswa</span>
+                </a>
+            </li>
+            <li class="{{ Request::is('*/input-nilai/*') || Request::is('*/input-nilai') ? 'active' : '' }}">
+                <a class="nav-link" href="#"><i class="fas fa-tasks"></i>
+                    <span>Input Nilai</span>
+                </a>
+            </li>
+
         </ul>
     </aside>
 </div>
