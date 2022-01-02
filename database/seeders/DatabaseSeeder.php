f638335c7d9e8fda2a4fa4738bf0a2ab754e8f27
Alvin <maulanaalvin1@gmail.com>
2022-01-03 03:00:56 +0800

    fix masalah seeder bermasalah

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             DosenSeeder::class,
             PegawaiSeeder::class,
             MahasiswaSeeder::class,
+            TahunAjaranSeeder::class,
             MatakuliahSeeder::class
         ]);
     }
