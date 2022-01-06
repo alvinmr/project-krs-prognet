ba4f2db3cf1d5eab3fe444c26a02540eb0e0382d
Alvin <maulanaalvin1@gmail.com>
2022-01-07 03:31:25 +0800

    make new mirgation message

--- database/migrations/2022_01_06_173536_create_message_mahasiswa_table.php
+++ database/migrations/2022_01_06_173536_create_message_mahasiswa_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMessageTable extends Migration
+class CreateMessageMahasiswaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,9 +15,10 @@ class CreateMessageTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->longText('pesan');
-            $table->foreignId('pegawai_id')->constrained('pegawai');
-            $table->foreignId('mahasiswa_id')->constrained('mahasiswa');
+            $table->longText('message')->nullable();
+            $table->foreignId('from_id');
+            $table->foreignId('to_id');
+            $table->timestamps();
         });
     }
 
