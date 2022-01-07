1e3a581d5ed61821af1d7d87d7b9a1d40989384f
Alvin <maulanaalvin1@gmail.com>
2022-01-07 23:12:12 +0800

    refactor: grouping routes

--- routes/web.php
+++ routes/web.php
@@ -29,46 +29,51 @@ Route::get('/', function () {
 });
 
 Route::prefix('mahasiswa/')->name('mahasiswa.')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('mahasiswa.login');
+    });
+
     Route::middleware('guest')->group(function () {
         Route::get('login', [MahasiswaController::class, 'showLogin'])->name('showLogin');
         Route::post('login', [MahasiswaController::class, 'login'])->name('login');
     });
-    Route::get('/', function () {
-        return redirect()->route('mahasiswa.login');
+
+
+    Route::middleware('auth:mahasiswa')->group(function () {
+        Route::post('logout', [MahasiswaController::class, 'logout'])->name('logout');
+        Route::get('dashboard', [MahasiswaController::class, 'dashboard'])->name('dashboard');
+
+        /* krs */
+        Route::get('krs', [KRSController::class, 'showTableKRS'])->name('krs-index');
+
+        /* krs detail */
+        Route::get('krs/detail/{id}', [KRSController::class, 'showDetailMatakuliah'])->name('krs-detail', 'id');
+
+        /* krs-create */
+        Route::get('krs/create', [KRSController::class, 'showCreateTableKRS'])->name('krs-create');
+        Route::post('krs/store/{id}', [KRSController::class, 'storeKRS'])->name('krs-store', 'id');
+
+        /* krs-edit */
+        Route::get('krs/edit', [KRSController::class, 'showEditTableKRS'])->name('krs-edit');
+        Route::post('krs/storeedit', [KRSController::class, 'storeEditTableKRS'])->name('krs-store-edit');
+
+        /* krs-delete */
+        Route::get('krs/delete', [KRSController::class, 'showDeleteTableKRS'])->name('krs-delete');
+        Route::post('krs/storedelete/{id}', [KRSController::class, 'storeDeleteTableKRS'])->name('krs-store-delete', 'id');
+
+        Route::resource('khs', KHSMahasiswaController::class);
+        Route::get('chat/{id}', [ChatController::class, 'index'])->name('chat.index');
+        Route::get('matakuliah/list-mahasiswa/{id}', [KRSController::class, 'listMahasiswa'])->name('krs-list-mahasiswa');
     });
-    Route::post('logout', [MahasiswaController::class, 'logout'])->name('logout');
-    Route::get('dashboard', [MahasiswaController::class, 'dashboard'])->name('dashboard')->middleware('auth:mahasiswa');
-
-    /* krs */
-    Route::get('krs', [KRSController::class, 'showTableKRS'])->name('krs-index')->middleware('auth:mahasiswa');
-
-    /* krs detail */
-    Route::get('krs/detail/{id}', [KRSController::class, 'showDetailMatakuliah'])->name('krs-detail', 'id')->middleware('auth:mahasiswa');
-
-    /* krs-create */
-    Route::get('krs/create', [KRSController::class, 'showCreateTableKRS'])->name('krs-create')->middleware('auth:mahasiswa');
-    Route::post('krs/store/{id}', [KRSController::class, 'storeKRS'])->name('krs-store', 'id')->middleware('auth:mahasiswa');
-
-    /* krs-edit */
-    Route::get('krs/edit', [KRSController::class, 'showEditTableKRS'])->name('krs-edit')->middleware('auth:mahasiswa');
-    Route::post('krs/storeedit', [KRSController::class, 'storeEditTableKRS'])->name('krs-store-edit')->middleware('auth:mahasiswa');
-
-    /* krs-delete */
-    Route::get('krs/delete', [KRSController::class, 'showDeleteTableKRS'])->name('krs-delete')->middleware('auth:mahasiswa');
-    Route::post('krs/storedelete/{id}', [KRSController::class, 'storeDeleteTableKRS'])->name('krs-store-delete', 'id')->middleware('auth:mahasiswa');
-
-    Route::resource('khs', KHSMahasiswaController::class);
-    Route::get('chat/{id}', [ChatController::class, 'index'])->name('chat.index');
-    Route::get('matakuliah/list-mahasiswa/{id}', [KRSController::class, 'listMahasiswa'])->name('krs-list-mahasiswa');
 });
 
 Route::prefix('pegawai/')->name('pegawai.')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('pegawai.login');
+    });
     Route::middleware('guest')->group(function () {
         Route::get('login', [PegawaiController::class, 'showLogin'])->name('showLogin');
         Route::post('login', [PegawaiController::class, 'login'])->name('login');
-    });
-    Route::get('/', function () {
-        return redirect()->route('pegawai.login');
     });
     Route::middleware('auth:pegawai')->group(function () {
         Route::post('logout', [PegawaiController::class, 'logout'])->name('logout');
