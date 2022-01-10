c4a51450ffd6ee1cffed6530ee50c32a6c65e809
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-01-11 03:21:29 +0800

    add file to print pdf

---
+++ resources/views/layouts/pdf.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+</body>
+
+</html>
